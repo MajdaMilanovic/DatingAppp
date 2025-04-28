8433431b0c56d6ae12c906751846bc9c42a6e570
MajdaMilanovic <majda.milanovic@edu.fit.ba>
2025-04-28 16:45:56 +0200

    AngularForms

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a89849cda975ed8255e6a01646b11164ae583a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+428a709d387964d84b1db68fca1f91fc98ae3ad2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
