08e2a3d9b4bda908f0f2211d7c49326eeafb161d
MajdaMilanovic <majda.milanovic@edu.fit.ba>
2025-04-28 23:35:19 +0200

    Client login and register

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
 model: any = {};
+private accountService = inject(AccountService);
+loggedIn =false;
 
 login()
 {
-  console.log(this.model);
+  this.accountService.login(this.model).subscribe({
+    next: response => {
+      console.log(response);
+      this.loggedIn =true;
+    },
+    error: error => console.log(error)
+  })
+}
+logout ()
+{
+  this.loggedIn =false;
 }
 
 }
