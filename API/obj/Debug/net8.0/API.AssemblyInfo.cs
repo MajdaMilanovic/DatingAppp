ed5b02ac52a070095a36177c32a06232396c28c2
MajdaMilanovic <majda.milanovic@edu.fit.ba>
2025-04-29 13:43:35 +0200

    Registration

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08e2a3d9b4bda908f0f2211d7c49326eeafb161d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224e59d268278dca8313c59688edc1292344f51f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -11,6 +11,7 @@ export class AccountService {
   private http = inject(HttpClient);
   baseUrl = "https://localhost:5001/api/";
   currentUser = signal<User | null>(null);
+ 
 
   login(model:any)  //setting signal
   {
@@ -21,6 +22,19 @@ export class AccountService {
           this.currentUser.set(user);        }
       })
     )
+  }
+
+  register(model:any) 
+  {
+    return this.http.post<User>(this.baseUrl + "account/register", model).pipe(
+      map(user => {
+        if(user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);}
+          return user;
+      })
+    )
+  
   }
 
   logout() {
