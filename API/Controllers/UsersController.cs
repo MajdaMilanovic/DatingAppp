3815981272231121a2549910f8ec4346510fd3fb
MajdaMilanovic <majda.milanovic@edu.fit.ba>
2025-05-04 15:03:31 +0200

    Update API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Controllers;
 using API.Data;
 using API.DTOs;
@@ -11,7 +12,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
 
     [HttpGet]
@@ -33,5 +34,20 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
         return user;
     }
 
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto) 
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if(username == null) return BadRequest("No username found");
+        var user = await userRepository.GetUserByUsernameAsync(username);
+
+        if(user == null) return BadRequest("Could not find user");
+        mapper.Map(memberUpdateDto, user);
+
+        if(await userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update the user");
+    } 
 
 }
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a85158d12579652b5fbc1c5b79719effb8f6671")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eb9a74013363980c37e43a5ce2a3644413a781f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
