1feedfa0f0e90e562e6a8bcae5c10d03d32007aa
MajdaMilanovic <majda.milanovic@edu.fit.ba>
2025-05-06 22:56:18 +0200

    API for messages

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34a459a695a66b7c168810f986d9866094b84643")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34308bc3854090e8a87a88ab275c8ac294b6bf8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
